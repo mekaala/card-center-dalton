6faf564899edb09809801b4fa6da97a701d8c7a4
mekaala <mekaala@gmail.com>
2020-06-30 16:25:33 -0400

    created separate diagnostic data for hospital services

---
+++ src/hospitalDiagnosticData.js
@@ -0,0 +1,43 @@
+export default
+[
+    {
+        name: 'Pacemaker Implantation',
+        diagnosticUrl: 'pacemaker',
+        offName: 'PACEMAKER IMPLANTATION',
+        about: 'A pacemaker is a device that sits under the skin near the heart that treats an abnormal heart rate. It regulates the electric system of the heart, allowing it to pump blood at a normal rate. Pacemakers send electrical signals to the heart, causing a slow or fast heartbeat to return to the normal rate. There are two parts to a pacemaker: the pulse generator - a thin, metal case with a battery and computer that creates and sends electrical signals, and leads - wires that connect the pulse generator to the heart.',
+        procedure: 'The doctor will give you a sedative to relax as well as an anesthetic to numb the incision site. You will be awake during the procedure. An incision will be made near your shoulder. A wire will be guided to a major vein in your collarbone, and make its way into the heart. An electrode from the wire will be attached to the right ventricle, and the other end will be attached to a pulse generator, which will be implanted under the skin near the collarbone. The incision will be closed with stitches after the implantation.',
+        warning: 'Do not consume any food or drink on the day of the surgery. The doctor will indicate which medicines to stop taking as they may interfere with the procedure. There may be medications that the doctor will prescribe for you before the test. Take them. To prevent serious infections, shower and shampoo well. You may be required to use a special body soap.'
+    },
+    {
+        name: 'Percutaneous Coronary Intervention',
+        diagnosticUrl: 'percutaneous-coronary-intervention',
+        offName: 'PERCUTANEOUS CORONARY INTERVENTION',
+        about: '',
+        procedure: '',
+        warning: '',
+    },
+    {
+        name: 'Coronary Artery Bypass Surgery',
+        diagnosticUrl: 'coronary-artery-bypass-surgery',
+        offName: 'CORONARY ARTERY BYPASS SURGERY',
+        about: '',
+        procedure: '',
+        warning: '',
+    },
+    {
+        name: 'Aortic Valve Replacement',
+        diagnosticUrl: 'aortic-valve-replacement',
+        offName: 'AORTIC VALVE REPLACEMENT',
+        about: '',
+        procedure: '',
+        warning: '',
+    },
+    {
+        name: 'Transcatheter Aortic Valve Replacement',
+        diagnosticUrl: 'transcatheter-aortic-valve-replacement',
+        offName: 'TRANSCATHETER AORTIC VALVE REPLACEMENT',
+        about: '',
+        procedure: '',
+        warning: '',
+    },
+]
