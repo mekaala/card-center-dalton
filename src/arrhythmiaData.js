f6e28a0c953c060b36419bd7b1ac476d82d014e3
mekaala <mekaala@gmail.com>
2020-07-10 15:33:31 -0400

    added more data sheets to organize data

---
+++ src/arrhythmiaData.js
@@ -0,0 +1,28 @@
+export default
+[
+    {
+        name: 'Atrial Fibrillation',
+        conditionUrl: 'atrial-fibrillation',
+        offName: 'ATRIAL FIBRILLATION',
+    },
+    {
+        name: 'Atrial Flutter',
+        conditionUrl: 'atrial-flutter',
+        offName: 'ATRIAL FLUTTER',
+    },
+    {
+        name: 'Supraventricular Tachycardia',
+        conditionUrl: 'supraventricular-tachycardia',
+        offName: 'SUPRAVENTRICULAR TACHYCARDIA',
+    },
+    {
+        name: 'Ventricular Tachycardia',
+        conditionUrl: 'ventricular-tachycardia',
+        offName: 'VENTRICULAR TACHYCARDIA',
+    },
+    {
+        name: 'Ventricular Flutter',
+        conditionUrl: 'ventricular-flutter',
+        offName: 'VENTRICULAR FLUTTER',
+    },
+]
---
+++ src/heartBlockData.js
@@ -0,0 +1,18 @@
+export default
+[
+    {
+        name: 'First Degree AV Block',
+        conditionUrl: 'first-degree-avblock',
+        offName: 'FIRST DEGREE AV BLOCK',
+    },
+    {
+        name: 'Second Degree AV Block',
+        conditionUrl: 'second-degree-avblock',
+        offName: 'SECOND DEGREE AV BLOCK',
+    },
+    {
+        name: 'Third Degree AV Block',
+        conditionUrl: 'third-degree-avblock',
+        offName: 'THIRD DEGREE AV BLOCK',
+    },
+]
