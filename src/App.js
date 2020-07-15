093104bcc25c814d00679fa37e2a4de14e3c817a
mekaala <mekaala@gmail.com>
2020-07-15 16:50:09 -0400

    modified routing with new scroll component

--- src/App.js
+++ src/App.js
@@ -13,25 +13,27 @@ import Services from './components/Services';
 import SingleService from './components/SingleService';
 import Contact from './components/Contact';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 function App() {
   return (
     <div className="App">
       <Router>
-        <NavBar/>
-        <Switch>
-          <Route exact path="/" component={ SplashPage }/>
-          <Route exact path="/staff" component={ Staff }/>
-          <Route path="/staff/:providerUrl" component={ SingleStaff }/>
-          <Route exact path="/conditions" component={ Conditions }/>
-          <Route path="/conditions/:conditionUrl" component={ SingleCondition }/>
-          <Route exact path="/diagnostics" component={ Diagnostics }/>
-          <Route path="/diagnostics/:diagnosticUrl" component={ SingleDiagnostic }/>
-          <Route exact path="/services" component={ Services }/>
-          <Route path="/services/:serviceUrl" component={ SingleService }/>
-        </Switch>
-        <Contact/>
-        <Footer/>
+        <ScrollToTop/>
+          <NavBar/>
+          <Switch>
+            <Route exact path="/" component={ SplashPage }/>
+            <Route exact path="/staff" component={ Staff }/>
+            <Route path="/staff/:providerUrl" component={ SingleStaff }/>
+            <Route exact path="/conditions" component={ Conditions }/>
+            <Route path="/conditions/:conditionUrl" component={ SingleCondition }/>
+            <Route exact path="/diagnostics" component={ Diagnostics }/>
+            <Route path="/diagnostics/:diagnosticUrl" component={ SingleDiagnostic }/>
+            <Route exact path="/services" component={ Services }/>
+            <Route path="/services/:serviceUrl" component={ SingleService }/>
+          </Switch>
+          <Contact/>
+          <Footer/>
       </Router>
     </div>
   );
