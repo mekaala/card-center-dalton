d38bcb8a3b996b870d34f1dab1cc93f642567745
mekaala <mekaala@gmail.com>
2020-07-15 16:50:42 -0400

    edits to data from review with MD

--- src/hospitalDiagnosticData.js
+++ src/hospitalDiagnosticData.js
@@ -1,19 +1,11 @@
 export default
 [
     {
-        name: 'Pacemaker Implantation',
-        diagnosticUrl: 'pacemaker',
-        offName: 'PACEMAKER IMPLANTATION',
-        about: 'A pacemaker is a device that sits under the skin near the heart that treats an abnormal heart rate. It regulates the electric system of the heart, allowing it to pump blood at a normal rate. Pacemakers send electrical signals to the heart, causing a slow or fast heartbeat to return to the normal rate. There are two parts to a pacemaker: the pulse generator - a thin, metal case with a battery and computer that creates and sends electrical signals, and leads - wires that connect the pulse generator to the heart.',
-        procedure: 'The doctor will give you a sedative to relax as well as an anesthetic to numb the incision site. You will be awake during the procedure. An incision will be made near your shoulder. A wire will be guided to a major vein in your collarbone, and make its way into the heart. An electrode from the wire will be attached to the right ventricle, and the other end will be attached to a pulse generator, which will be implanted under the skin near the collarbone. The incision will be closed with stitches after the implantation.',
-        warning: 'Do not consume any food or drink on the day of the surgery. The doctor will indicate which medicines to stop taking as they may interfere with the procedure. There may be medications that the doctor will prescribe for you before the test. Take them. To prevent serious infections, shower and shampoo well. You may be required to use a special body soap.'
-    },
-    {
         name: 'Cardiac Catheterization',
         diagnosticUrl: 'cardiac-catheterization',
         offName: 'CARDIAC CATHETERIZATION',
         about: 'Cardiac catheterization is performed when doctors diagnose for certain heart problems, to understand heart performance, or to find underlying reasons for chest pain. The doctor looks at the arteries in the heart to find any blockages (coronary angiography), or find any problems in the heart chambers or valves.',
-        procedure: 'You will be awake during the procedure, but also will be administered medication to feel relaxed. An incision will be made, either at the top, inner part of your leg, or at your wrist, after being numbed. A tube will enter your heart through the incision site and the blood vessels. An X-ray will inform the doctor of when the tube reaches its destination where tests will be performed. For coronary angiography, a red dye will be injected, showing any blockages in the arteries. After the procedure, you will need to rest in the hospital for a few hours, and reschedule a follow up appointment regarding the results.',
+        procedure: 'You will be awake during the procedure, but also will be administered medication to feel relaxed. An arterial puncture will be made, either at the top, inner part of your leg, or at your wrist, after being numbed. A tube will enter your heart through the artery puncture site and the blood vessels. An X-ray will inform the doctor of when the tube reaches its destination where tests will be performed. For coronary angiography, a contrast dye will be injected, showing any blockages in the arteries. After the procedure, you will need to rest in the hospital for a few hours, and be scheduled a follow up appointment.',
         warning: 'Do not consume food after midnight during the night before the procedure. It is unlikely, but you may be advised to avoid taking certain medications. The doctor will inform you of any medications that should not be administered. You will be informed if you need someone else to drive you to and from the hospital, and you may stay overnight if any artery repair is performed.'
     },
     {
@@ -21,11 +13,11 @@ export default
         diagnosticUrl: 'percutaneous-coronary-intervention',
         offName: 'PERCUTANEOUS CORONARY INTERVENTION (PCI)',
         about: 'Also known as a coronary angioplasty, percutaneous coronary intervention (PCI) is a minimally invasive and nonsurgical procedure used to improve blood flow of segments around the heart area. It generally uses stents to open and sustain blood flow by inflating a balloon in the clogged artery to expand the vessel. PCI is performed to prevent chest pain and heart attacks due to blocked arteries.',
-        procedure: 'PCI is performed through an artery in the groin, arm, or wrist, generally without anesthesia. An IV catheter will be placed in your hand and arm to provide fluids and medications to relax you. A local anesthetic will numb the area of incision. Contrast dye will indicate clogged arteries on an X-ray, and a thin wire with the balloon will be inserted into the blood vessel. The balloon will be inflated at the site of blockage, stretching the artery. Upon completion, the balloon deflates, and the catheter is removed. A stent may be implanted to keep the artery open',
+        procedure: 'PCI is performed through an artery in the groin, arm, or wrist, generally without anesthesia. An IV catheter will be placed in your hand and arm to provide fluids and medications to relax you. A local anesthetic will numb the area of incision. Contrast dye will indicate clogged arteries on an X-ray, and a thin wire with the balloon will be inserted into the blood vessel. The balloon will be inflated at the site of blockage, stretching the artery. Upon completion, the balloon deflates, and a stent is implanted to keep the artery open.',
         warning: 'Avoid food or drink for at least six hours before the procedure. The doctor will indicate any medications to avoid taking as they may interfere with the procedure. Arrange for transportation to and from the hospital as PCI may require an overnight stay, and you may not be able to drive home.',
     },
     {
-        name: 'Coronary Artery Bypass Surgery',
+        name: 'Coronary Artery Bypass Surgery (CABG)',
         diagnosticUrl: 'coronary-artery-bypass-surgery',
         offName: 'CORONARY ARTERY BYPASS SURGERY',
         about: 'Coronary artery bypass surgery treats coronary heart disease (where the arteries connected to the heart are clogged by fatty deposits, blocking blood flow). The surgeon creates grafts from healthy blood vessels from other parts of the body that are attached above and below the blockage. This creates an alternate route for blood to flow around the blockage. They can also redirect an artery from the chest muscle to the heart. Coronary artery bypass surgery is mainly recommended to patients with chest pain that does not resolve with medication or stenting, or blockages that may result in a heart attack or death.',
@@ -33,20 +25,12 @@ export default
         warning: 'Coronary artery bypass surgery is intensive, please follow all precautions. Do not consume food or drink after midnight on the day of the procedure. The doctor will inform you of certain medications that should not be taken at least several days before the surgery. More detailed preparations will be given when scheduling the procedure. After the surgery, you will spend 1 to 2 days in the ICU, regaining strength. The wound will be sore for 2 to 3 days more, but pain medications will help.',
     },
     {
-        name: 'Surgical Aortic Valve Replacement',
-        diagnosticUrl: 'surgical-aortic-valve-replacement',
-        offName: 'SURGICAL AORTIC VALVE REPLACEMENT',
-        about: 'SAVR involves the removal of the narrowed valve and replacement with a new one. The new valve is made from metal or animal tissue. Damaged valves arise due to aortic stenosis, when the aortic valve does not open fully, limiting blood flow. It is a more helpful procedure for children and young adults.',
-        procedure: '',
-        warning: '',
-    },
-    {
-        name: 'Transcatheter Aortic Valve Replacement',
-        diagnosticUrl: 'transcatheter-aortic-valve-replacement',
-        offName: 'TRANSCATHETER AORTIC VALVE REPLACEMENT',
-        about: 'Transcatheter aortic valve replacement (TAVR) involves the insertion of a new aortic valve without removing the damaged valve. Damaged valves arise due to aortic stenosis, when the aortic valve does not open fully, limiting blood flow. TAVR uses balloons to inflate the valve, allowing blood flow to resume its ideal rate. There are usually no symptoms reported for early aortic stenosis, but heart murmurs detected in routine exams can be a sign of the condition. Symptoms of aortic stenosis such as shortness of breath, dizziness or fainting, and chest pain can appear during physical activity.',
-        procedure: 'You will be administered anesthesia at the site of incision, and will be conscious during the procedure. Just like with cardiac catheterization, a wire will approach the valve through a vessel in the arm or leg that leads to the heart. A coronary angiography is performed to diagnose if you are suffering from aortic stenosis by injecting dye that highlights narrowed or blocked arteries on an X-ray. The doctor will inflate a balloon in the narrowed valve to expand it.',
-        warning: 'Do not consume food after midnight during the night before the procedure. It is unlikely, but you may be advised to avoid taking certain medications. The doctor will inform you of any medications that should not be administered. You will be informed if you need someone else to drive you to and from the hospital, and you may stay overnight if any artery repair is performed.',
+        name: 'Pacemaker Implantation',
+        diagnosticUrl: 'pacemaker',
+        offName: 'PACEMAKER IMPLANTATION',
+        about: 'A pacemaker is a device that sits under the skin near the heart that treats an abnormal heart rate. It regulates the electric system of the heart, allowing it to pump blood at a normal rate. Pacemakers send electrical signals to the heart, causing a slow or fast heartbeat to return to the normal rate. There are two parts to a pacemaker: the pulse generator - a thin, metal case with a battery and computer that creates and sends electrical signals, and leads - wires that connect the pulse generator to the heart.',
+        procedure: 'The doctor will give you a sedative to relax as well as an anesthetic to numb the incision site. You will be awake during the procedure. An incision will be made near your shoulder. A wire will be guided to a major vein in your collarbone, and make its way into the heart. An electrode will be attached to the right ventricle, and the other end will be attached to a pulse generator, which will be implanted under the skin near the collarbone. The incision will be closed with stitches after the implantation.',
+        warning: 'Do not consume any food or drink on the day of the surgery. The doctor will indicate which medicines to stop taking as they may interfere with the procedure. There may be medications that the doctor will prescribe for you before the test. Take them. To prevent serious infections, shower and shampoo well. You may be required to use a special body soap.'
     },
     {
         name: 'Implantable Loop Recorder',
@@ -55,5 +39,21 @@ export default
         about: 'The ILR monitors the electrical activity of the heart over an extended period rather than the 24-48 hours by the Holter Monitor and the 30-day Event Monitor. It evaluates recurrent palpitations, syncope, or other conditions that are not discovered with other monitors.',
         procedure: 'A small incision is made on the left side at about the level of the second or third rib. A pocket is created under the skin, and the device is inserted in that pocket, and then the incision is closed. The device is usually implanted by an electrophysiologist as an outpatient procedure using local anesthesia. Conscious sedation can be used but is generally not required. Once implanted, the device is activated by passing a magnet over it.',
         warning: 'It is advised that you refrain from eating or drinking after midnight before your procedure. You will be informed if any medications should not be taken before the implantation.',
+    },
+    {
+        name: 'Surgical Aortic Valve Replacement (SAVR)',
+        diagnosticUrl: 'surgical-aortic-valve-replacement',
+        offName: 'SURGICAL AORTIC VALVE REPLACEMENT',
+        about: 'Surgical aortic valve replacement (SAVR) involves the removal of the narrowed valve and replacement with a new one. The new valve is made from metal or animal tissue. Damaged valves result in aortic stenosis, in which the aortic valve does not open fully, leading to regurgitation.',
+        procedure: '',
+        warning: '',
+    },
+    {
+        name: 'Transcatheter Aortic Valve Replacement (TAVR)',
+        diagnosticUrl: 'transcatheter-aortic-valve-replacement',
+        offName: 'TRANSCATHETER AORTIC VALVE REPLACEMENT',
+        about: 'Transcatheter aortic valve replacement (TAVR) involves the insertion of a new aortic valve without removing the damaged valve. Damaged valves arise due to aortic stenosis, when the aortic valve does not open fully, limiting blood flow. TAVR uses balloons to inflate the valve, allowing blood flow to resume its ideal rate. There are usually no symptoms reported for early aortic stenosis, but heart murmurs detected in routine exams can be a sign of the condition. Symptoms of aortic stenosis such as shortness of breath, dizziness or fainting, and chest pain can appear during physical activity.',
+        procedure: 'You will be administered anesthesia at the site of incision, and will be conscious during the procedure. Just like with cardiac catheterization, a wire will approach the valve through a vessel in the arm or leg that leads to the heart. A coronary angiography is performed to diagnose if you are suffering from aortic stenosis by injecting dye that highlights narrowed or blocked arteries on an X-ray. The doctor will inflate a balloon in the narrowed valve to expand it.',
+        warning: 'Do not consume food after midnight during the night before the procedure. It is unlikely, but you may be advised to avoid taking certain medications. The doctor will inform you of any medications that should not be administered. You will be informed if you need someone else to drive you to and from the hospital, and you may stay overnight if any artery repair is performed.',
     },
 ]
--- src/providerData.js
+++ src/providerData.js
@@ -6,8 +6,8 @@ export default
         description: 'Dr. Ezad N. Ahmad completed his internal medicine residency at Wayne State University School of Medicine (Detroit, MI) in 1988, followed by a cardiology fellowship at the University of Wisconsin Hospital in 1991. A fellow of the American College of Cardiology (FACC), Dr. Ahmad is board certified in internal medicine, cardiovascular disease and cardiac angiography. Dr. Ahmad began his career in medicine in private practice in Virginia before moving to Georgia in 1995, where he established his private practice in Dalton. He is an expert in diagnostic cardiac catheterization, echocardiography, pacemaker implantation, nuclear cardiology and cardiac angiography.',
         photo: require('./Staff/ezad-n-ahmad.jpg'),
         md: 'Sind Medical College, University of Karachi, Karachi, Pakistan',
-        residency: 'Wayne State University School of Medicine, Detroit, Michigan',
-        fellowship: 'Cardiology, University of Wisconsin Hospital School of Medicine, Madison, Wisconsin',
+        residency: 'Internal Medicine, Wayne State University School of Medicine, Detroit, Michigan',
+        fellowship: 'Cardiology Fellowship, University of Wisconsin Hospital School of Medicine, Madison, Wisconsin',
         board: 
         [
             'American Board of Internal Medicine ',
@@ -23,7 +23,6 @@ export default
         hospital: 
         [
             'Hamilton Medical Center, Dalton, Georgia ',
-            'Memorial Hospital, Chattanooga, Tennessee'
         ]
     }
 ]
