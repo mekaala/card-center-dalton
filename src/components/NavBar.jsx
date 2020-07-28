8bbf740346cefe6e00dcddf87dbedabc68bb176f
mekaala <mekaala@gmail.com>
2020-07-27 23:52:49 -0400

    styled a dropdown bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -13,14 +13,15 @@ export default class NavBar extends Component {
             showConditions: newShowConditions,
         });
     }
+
     render() {
         return (
             <div className="navbar">
                 <Link className="logo" to="/"><img src={ logo } alt="logo"/></Link>
                 <div className="bar">
                     <Link to="/">Home</Link>
-                    <div className="condition-menu">
-                        <Link onMouseOver={ this.toggleConditions }>
+                    <div className="condition-menu" onMouseEnter={ this.toggleConditions } onMouseLeave={ this.toggleConditions }>
+                        <Link>
                             { this.state.showConditions
                                 ? 'Conditions'
                                 : 'Conditions'
@@ -28,14 +29,14 @@ export default class NavBar extends Component {
                         </Link>
                         { this.state.showConditions
                         ?
-                            <div className="dropdown-conditions" onMouseLeave={ this.toggleConditions }>
+                            <div className="dropdown-conditions">
                                 <Link to="/conditions">All Conditions</Link>
                                 <Link to="/conditions/diseases">Diseases</Link>
-                                <Link to="/conditions/arrythmias">Arrythmia</Link>
+                                <Link to="/conditions/arrythmias">Arrythmias</Link>
                                 <Link to="/conditions/heart-block">Heart Block</Link>
                                 <Link to="/conditions/symptoms">Symptoms</Link>
                             </div>
-                        : null
+                        : <div className="collapse-conditions"/>
                         }
                     </div>
                     <Link to="/diagnostics">Diagnostics</Link>
