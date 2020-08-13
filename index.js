e79f46a0f12731999c38de8a8e3c74ae16217912
mekaala <mekaala@gmail.com>
2020-08-13 19:21:32 -0400

    not even close

--- index.js
+++ index.js
@@ -9,17 +9,8 @@ dotenv.config();
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.json());
 app.use(cors());
+app.use(express.static(`${__dirname}/client/build`))
 
-if (process.env.NODE_ENV === 'production') {
-  // Exprees will serve up production assets
-  app.use(express.static('client/build'));
-
-  // Express serve up index.html file if it doesn't recognize route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-  });
-}
 
 app.post('/send', (req, res) => {
     try {
