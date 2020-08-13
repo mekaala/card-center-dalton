734c96f7582744892a5ee69f5b35be1643d97be9
mekaala <mekaala@gmail.com>
2020-08-13 18:38:59 -0400

    modified server stuff

--- index.js
+++ index.js
@@ -59,6 +59,9 @@ app.post('/send', (req, res) => {
 });
   });
 
-app.listen(3001, () => {
-  console.log('server start on port 3001');
-});
+  app.set( 'port', ( process.env.PORT || 3001 ));
+
+  // Start node server
+  app.listen( app.get( 'port' ), function() {
+    console.log( 'Node server is running on port ' + app.get( 'port' ));
+    });
