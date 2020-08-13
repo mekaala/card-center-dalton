5a36daf319a0f6e92549bd4c2c5a8dbb718a3c21
mekaala <mekaala@gmail.com>
2020-08-13 18:46:06 -0400

    index updates - please be the end

--- index.js
+++ index.js
@@ -8,8 +8,18 @@ dotenv.config();
 
 const buildPath = path.join(__dirname, '..', 'build');
 app.use(express.json());
-app.use(express.static(buildPath));
 app.use(cors());
+
+if (process.env.NODE_ENV === 'production') {
+  // Exprees will serve up production assets
+  app.use(express.static('client/build'));
+
+  // Express serve up index.html file if it doesn't recognize route
+  const path = require('path');
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
 
 app.post('/send', (req, res) => {
     try {
