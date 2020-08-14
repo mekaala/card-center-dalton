328c807ba018afe0b8c99b82b9e2cc48f8b579e4
mekaala <mekaala@gmail.com>
2020-08-14 15:45:42 -0400

    fixed up things

--- client/src/components/AppointmentForm.jsx
+++ client/src/components/AppointmentForm.jsx
@@ -39,15 +39,22 @@ export default class AppointmentForm extends Component {
         Axios.post('/send', this.state.newAppointment)
         .then(() => {
             this.setState({
-                redirect: true,
+                buttonText: 'Sent!',
             });
+            form.style.display = 'none';
+            booked.style.display = 'block';
         });
+    }
+    returnHome() {
+        this.setState({
+            redirect: true,
+        })
     }
     render() {
         const newAppointment = this.state.newAppointment;
         if (this.state.redirect) {
             return <Redirect to="/"/>
-        };
+        }
         return (
             <div className="appointment-form">
                 <h1>Appointment Request Form</h1>
@@ -82,12 +89,13 @@ export default class AppointmentForm extends Component {
                         <div className="patient-information">
                             <div><label>What can we help you with? Please state if you are feeling chest pain, experiencing symptoms of heart disease, or if you want a check up. *</label><br/><textarea required type="text" name="description" onChange={ this.changeInput } value = { newAppointment.description }/></div>
                         </div>
-                        <div className="submit-button"><input type="submit" value={ this.state.buttonText } className="g-recaptcha" data-sitekey="6LcvBb4ZAAAAAKKDKPrhw4IKYPtyACO8ekgMLfHr" data-callback={this.onSubmit} data-action='submit'/></div>
+                        <div className="submit-button"><input type="submit" value={ this.state.buttonText } className="g-recaptcha" data-sitekey="6LcvBb4ZAAAAAKKDKPrhw4IKYPtyACO8ekgMLfHr" data-callback={ this.onSubmit } data-action='submit'/></div>
                     </div>
-                    {/* <div id='booked'>
+                    <div id='booked'>
                         <h3>Thank you!</h3>
                         <p>Your request has been sent to the office. You will receive a confirmation email at the address provided. We will contact you within two business days.</p>
-                    </div> */}
+                        <button onClick={ this.returnHome }>Return Home</button>
+                    </div>
                 </form>
             </div>
         )
