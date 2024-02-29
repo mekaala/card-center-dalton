b255d2a7e666b60d97c452bb216a28ead5f2666a
mekaala <mekaala@gmail.com>
2024-02-29 09:25:36 -0500

    converted to functional components

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -1,22 +1,61 @@
 import React, { Component } from 'react'
 
-export default class Contact extends Component {
-    render() {
-        return (
-            <div className="contact">
-                <div className="contact-info">
-                    <h1>Cardiology Center of Dalton</h1>
-                    <h4>
-                        Monday - Friday<br/>
-                        9:00 am to 5:00 pm
-                    </h4>
-                    <h4>
-                        1411 Chattanooga Avenue<br/>
-                        Dalton, Georgia 30710
-                    </h4>
-                    <h4>706-272-0272</h4> 
-                </div>
+function Contact() {
+  return (
+    <div className="contact">
+        <h1>Cardiology Center of Dalton</h1>
+        <div className="contact-info">
+            <div className="hours">
+                <h2>Hours of Operation</h2>
+                <h4>
+                    Monday - Thursday<br/>
+                    9:00 am to 5:00 pm
+                    <br/>
+                    Friday<br/>
+                    9:00 am to 12:00 pm
+                </h4>
             </div>
-        )
-    }
+            <div className="address">
+                <h2>Address</h2>
+                <h4>
+                    1411 Chattanooga Avenue<br/>
+                    Dalton, Georgia 30710
+                </h4>
+            </div>
+            <div className="phone">
+                <h2>Phone Number</h2>
+                <h4>706-272-0272</h4> 
+            </div>
+        </div>
+    </div>
+  )
 }
+
+export default Contact
+
+// export default class Contact extends Component {
+//     render() {
+//         return (
+//             <div className="contact">
+//                 <h1>Cardiology Center of Dalton</h1>
+//                 <div className="contact-info">
+//                     <h2>Hours of Operation</h2>
+//                     <h4>
+//                         Monday - Thursday<br/>
+//                         9:00 am to 5:00 pm
+//                         <br/>
+//                         Friday<br/>
+//                         9:00 am to 12:00 pm
+//                     </h4>
+//                     <h2>Address</h2>
+//                     <h4>
+//                         1411 Chattanooga Avenue<br/>
+//                         Dalton, Georgia 30710
+//                     </h4>
+//                     <h2>Phone Number</h2>
+//                     <h4>706-272-0272</h4> 
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
--- client/src/components/SplashPage.jsx
+++ client/src/components/SplashPage.jsx
@@ -1,32 +1,41 @@
 import React, { Component } from 'react'
-import Activities from './Activities';
-import HealthKey from './HealthKey';
-import StressTestSplash from './StressTestSplash';
-import FoodShow from './FoodShow';
 import ShapeTag from './ShapeTag';
-import { Link } from 'react-router-dom';
 import Dieting from './Dieting';
-import Services from './Services';
-import Covid from './Covid';
 
-export default class SplashPage extends Component {
-    render() {
-        return (
-            <div className="splashpage">
-                <div className="image-page">
-                    <div className="image-text">
-                        <h1>CARDIOLOGY CENTER OF DALTON</h1>
-                    </div>
-                </div>
-                <div className="about">
-                    <h3>In our independently owned, state-of-the-art facility, our board-certified cardiologist and staff treat our patients like royalty. We take pride in the hands-on, compassionate care we offer to each and every patient, and we even offer bilingual interpretation.</h3>
-                </div>
-                <Covid/>
-                {/* <Services/> */}
-                <Dieting/>
-                {/* <HealthKey/> */}
-                <ShapeTag/>
+// export default class SplashPage extends Component {
+//     render() {
+//         return (
+//             <div className="splashpage">
+//                 <div className="image-page">
+//                     <div className="image-text">
+//                         <h1>CARDIOLOGY CENTER OF DALTON</h1>
+//                     </div>
+//                 </div>
+//                 <div className="about">
+//                     <h3>In our independently owned, state-of-the-art facility, our board-certified cardiologist and staff treat our patients like royalty. We take pride in the hands-on, compassionate care we offer to each and every patient, and we even offer bilingual interpretation.</h3>
+//                 </div>
+//                 <Dieting/>
+//                 <ShapeTag/>
+//             </div>
+//         )
+//     }
+// }
+
+// import React from 'react'
+
+function SplashPage() {
+    return (
+        <div className="splashpage">
+            <div className="image-page">
+                
             </div>
-        )
-    }
+            <div className="about">
+                <h3>In our independently owned, state-of-the-art facility, our board-certified cardiologist and staff treat our patients like royalty. We take pride in the hands-on, compassionate care we offer to each and every patient, and we even offer bilingual interpretation.</h3>
+            </div>
+            <Dieting/>
+            <ShapeTag/>
+        </div>
+    )
 }
+
+export default SplashPage
