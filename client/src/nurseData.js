d6e61c86ce3db7b21fcfbba810bfc929509e79b8
mekaala <mekaala@gmail.com>
2024-02-29 09:26:14 -0500

    updating data with new nurse

--- client/src/nurseData.js
+++ client/src/nurseData.js
@@ -1,8 +1,8 @@
 export default
 [
     {
-        name: 'Allison McBride, APRN, MSN, FNP-C',
-        url: '/staff/allison-mcbride',
+        name: 'Stephanie Jinright, NP',
+        url: '/staff/stephanie-jinright',
         photo: require('./Staff/allison-mcbride.jpg'),
         licensure: 
         [
@@ -16,49 +16,4 @@ export default
             'King University - BSN, Bristol, Tennessee'
         ]
     },
-    // {
-    //     name: 'Allison McBride, APRN, MSN, FNP-C',
-    //     url: '/staff/allison-mcbride',
-    //     photo: require('./Staff/allison-mcbride.jpg'),
-    //     licensure: 
-    //     [
-    //         'Family Nurse Practicioner, NP-C, Certification No. F06182565 ',
-    //         'Registered Nurse, State of Tennessee/Multistate, No. 173164 ',
-    //         'Registered Nurse, State of Georgia, No. RN271505',
-    //     ],
-    //     education: 
-    //     [
-    //         'Southern Adventist University - MSN, FNP, Collegedale, Tennessee ',
-    //         'King University - BSN, Bristol, Tennessee'
-    //     ]
-    // },
-    // {
-    //     name: 'Victoria Alexus Smith, RN, MSN',
-    //     url: '/staff/victoria-alexus-smith',
-    //     photo: require('./Staff/victoria-alexus-smith.jpg'),
-    //     licensure:
-    //     [
-    //         'Registered Nurse, Tennessee Multistate, Certification No. 221928 ',
-    //     ],
-    //     education:
-    //     [
-    //         'East Tennessee State University - MSN, Johnson City, Tennessee ',
-    //         'Tennessee Wesleyan University - BSN, Athens, Tennessee',
-    //     ],
-    // },
-    // {
-    //     name: 'Rachel Krumrie, CCRN',
-    //     url: '/staff/rachel-krumrie',
-    //     photo: require('./Staff/rachel-krumrie.jpg'),
-    //     licensure: 
-    //     [
-    //         'Registered Nurse, State of Tennessee, No. 185609 ',
-    //         'Registered Nurse, State of Georgia, No. RN271505 ',
-    //     ],
-    //     education: 
-    //     [
-    //         'Southern Adventist University - MSN, Collegedale, Tennessee',
-    //         'University of Alabama - BSN, Tuscalosa, Alabama'
-    //     ]
-    // },
 ]
