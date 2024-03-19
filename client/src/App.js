7830348694ec74c7877645a8f180560c2d9ba136
mekaala <mekaala@gmail.com>
2024-03-19 12:27:20 -0400

    removed mobile bar component

--- client/src/App.js
+++ client/src/App.js
@@ -24,7 +24,6 @@ import Footer from './components/Footer';
 import ScrollToTop from './components/ScrollToTop';
 import AppointmentRequest from './components/AppointmentRequest';
 import AppointmentForm from './components/AppointmentForm';
-import MobNavBar from './components/MobNavBar';
 
 
 function App() {
@@ -32,7 +31,6 @@ function App() {
     <div className="App">
       <Router>
         <ScrollToTop/>
-          <MobNavBar/>
           <NavBar/>
           <Switch>
             <Route exact path="/" component={ SplashPage }/>
