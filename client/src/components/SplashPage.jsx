e23b46b77d9b26c953bb79e65a78cb0d440efcdb
mekaala <mekaala@gmail.com>
2024-03-21 16:35:15 -0400

    added alt text

--- client/src/components/SplashPage.jsx
+++ client/src/components/SplashPage.jsx
@@ -7,7 +7,7 @@ import office from '../images/office.JPG';
 function SplashPage() {
     return (
         <div className="splashpage">
-            <img className="image-page" src={ office }/>
+            <img className="image-page" src={office} alt='office image'/>
             <div className="about">
                 <h3>In our independently owned, state-of-the-art facility, our board-certified cardiologist and staff treat our patients like royalty. We take pride in the hands-on, compassionate care we offer to each and every patient, and we even offer bilingual interpretation.</h3>
             </div>
